e18d32b3b851ba8f2427778ae805cd9492a7ca41
nabil-github0 <nabilahmed.2977.2019@gmail.com>
2022-07-14 03:37:13 -0700

    Fixes some mobile responsiveness

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,7 @@ const Card =({name , email, id})=>{
         <div className="tc bg-green dib br3  grow bw2 ma3  shadow-5">
         <img src={`https://robohash.org/${id}?300x300`} alt="Robtos"/>
             <div>
-                <h1 className="f3">{name}</h1>
+                <h1 className="nabil">{name}</h1>
                 <p className="f4">{email}</p>
             </div>
         </div>
